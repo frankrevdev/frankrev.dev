20e6b1e4d672520d175bd4065e8d5006e540aa20
francopadua <franco.padua3301@gmail.com>
2026-04-14 21:40:35 +0800

    Added other links in contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,70 @@
+import './Contact.css';
+
+// Links
+const links = [
+    {
+        key: "insta",
+        name: "Instagram",
+        url: "https://www.instagram.com/frankrev.dev",
+        icon: new URL("../assets/Images/socials-links/insta.svg", import.meta.url).href
+    },
+    {
+        key: "threads",
+        name: "Threads",
+        url: "https://www.threads.com/@frankrev.dev",
+        icon: new URL("../assets/Images/socials-links/threads.svg", import.meta.url).href
+    },
+    {
+        key: "itch",
+        name: "Itch.io",
+        url: "https://frankrevdev.itch.io",
+        icon: new URL("../assets/Images/socials-links/itch.svg", import.meta.url).href
+    },
+    {
+        key: "yt",
+        name: "YouTube",
+        url: "https://www.youtube.com/@frankrevdev",
+        icon: new URL("../assets/Images/socials-links/yt.svg", import.meta.url).href
+    }
+];
+
+function LinkCard({ name, src, url }) {
+    return (
+        <a 
+            href={url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="linkCard"
+        >
+            <img src={src} alt={name} />
+            <span>{name}</span>
+        </a>
+    );
+}
+
 export default function Contact() {
     return (
         <>
             <title>frankrevdev | contact</title>
+
+            <div className="socialLinksContainer">
+                <div className="titleBar"></div>
+                <div className="titleContainer">
+                    <div className="titleContent">
+                        <span className="content-title links-title-full">Other Links</span>
+                    </div>
+                </div>
+                <div className="linksContent">
+                    {links.map((mlink) => (
+                        <LinkCard
+                            key={mlink.key}
+                            name={mlink.name}
+                            src={mlink.icon}
+                            url={mlink.url}
+                        />
+                    ))}
+                </div>
+            </div>
         </>
     );
 }
--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -17,10 +17,10 @@ export default function Works() {
                     </div>
                 </div>
                 <div className="game_assets_content">
-                    <iframe  width="552" height="167" frameborder="0" src="https://itch.io/embed/4364496">
+                    <iframe  width="552" height="167" frameBorder="0" src="https://itch.io/embed/4364496">
                         <a href="https://frankrevdev.itch.io/puzzledepizzle-v01">PuzzleDePizzle v0.1 by Franco Padua</a>
                     </iframe>
-                    <iframe width="552" height="167" frameborder="0" src="https://itch.io/embed/3754499">
+                    <iframe width="552" height="167" frameBorder="0" src="https://itch.io/embed/3754499">
                         <a href="https://frankrevdev.itch.io/actionemotional-bgm-1">[FREE] Action/Emotional 8/16/24/32 Bar Loop BGM #1 by Franco Padua</a>
                     </iframe>
                 </div>
@@ -36,12 +36,12 @@ export default function Works() {
                 </div>
                 <div className="musicContent">
                     <iframe src="https://open.spotify.com/embed/artist/3hD6jHhI80jHqBKcDHPqN4"
-                        width="300" height="380" frameborder="0"
+                        width="300" height="380" frameBorder="0"
                         allowtransparency="true" allow="encrypted-media">
                     </iframe>
 
                     <iframe src="https://open.spotify.com/embed/artist/74MbgZuZe4oshpldo6aRRL"
-                        width="300" height="380" frameborder="0"
+                        width="300" height="380" frameBorder="0"
                         allowtransparency="true" allow="encrypted-media">
                     </iframe>
                 </div>
