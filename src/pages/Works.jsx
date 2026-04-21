63ddb7eb765ec96a9b259719e4c1367ba9224a66
francopadua <franco.padua3301@gmail.com>
2026-04-21 20:55:23 +0800

    Modified Works page

--- src/pages/Works.jsx
+++ src/pages/Works.jsx
@@ -3,11 +3,25 @@ import './Works.css';
 import game_icon from "../assets/Images/p/works/icons/game-icon.png"
 import music_icon from "../assets/Images/p/works/icons/music-icon.png"
 
+function ItchIframe({width="552", height="167", src}) {
+    return (
+        <div className="itch-embed-wrapper">
+            <iframe
+                src={src}
+                loading="lazy"
+                frameBorder="0"
+                scrolling="no"
+            >
+            </iframe>
+        </div>
+    );
+}
+
 export default function Works() {
     return (
         <>
             <title>frankrevdev | works</title>
-            
+
             <div className="gamesAssetsContainer">
                 <div className="titleBar"></div>
                 <div className="titleContainer">
@@ -17,12 +31,16 @@ export default function Works() {
                     </div>
                 </div>
                 <div className="game_assets_content">
-                    <iframe  width="552" height="167" frameBorder="0" src="https://itch.io/embed/4364496">
-                        <a href="https://frankrevdev.itch.io/puzzledepizzle-v01">PuzzleDePizzle v0.1 by Franco Padua</a>
-                    </iframe>
-                    <iframe width="552" height="167" frameBorder="0" src="https://itch.io/embed/3754499">
-                        <a href="https://frankrevdev.itch.io/actionemotional-bgm-1">[FREE] Action/Emotional 8/16/24/32 Bar Loop BGM #1 by Franco Padua</a>
-                    </iframe>
+                    <div className="itch-embed-wrapper">
+                        <iframe width="70%" height="167" frameBorder="0" scrolling="no" src="https://itch.io/embed/4364496">
+                            <a href="https://frankrevdev.itch.io/puzzledepizzle-v01">PuzzleDePizzle v0.1 by Franco Padua</a>
+                        </iframe>
+                    </div>
+                    <div className="itch-embed-wrapper">
+                        <iframe width="70%" height="167" frameBorder="0" src="https://itch.io/embed/3754499">
+                            <a href="https://frankrevdev.itch.io/actionemotional-bgm-1">[FREE] Action/Emotional 8/16/24/32 Bar Loop BGM #1 by Franco Padua</a>
+                        </iframe>
+                    </div>
                 </div>
             </div>
 
@@ -36,12 +54,12 @@ export default function Works() {
                 </div>
                 <div className="musicContent">
                     <iframe src="https://open.spotify.com/embed/artist/3hD6jHhI80jHqBKcDHPqN4"
-                        width="300" height="380" frameBorder="0"
+                        width="500" height="380" frameBorder="0"
                         allowtransparency="true" allow="encrypted-media">
                     </iframe>
 
                     <iframe src="https://open.spotify.com/embed/artist/74MbgZuZe4oshpldo6aRRL"
-                        width="300" height="380" frameBorder="0"
+                        width="500" height="380" frameBorder="0"
                         allowtransparency="true" allow="encrypted-media">
                     </iframe>
                 </div>
